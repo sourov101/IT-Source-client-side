e8af03ccd53e79624d77e402be6fdb94f0c14c7a
sourov <sourov.jsr98@gmail.com>
2022-10-25 02:58:20 +0600

    login and logout toggle added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -27,24 +27,29 @@ const Header = () => {
                         <Link className="btn btn-ghost normal-case text-base" to='/courses'>Courses</Link>
                         <Link className="btn btn-ghost normal-case text-base" to='/faq'>FAQ</Link>
                         <Link className="btn btn-ghost normal-case text-base" to='/blog'>Blog</Link>
-                        <Link className="btn btn-ghost normal-case text-base" to='/login'>Login</Link>
+
                         <Link className="btn btn-ghost normal-case text-base" to='/register'>Register</Link>
 
                     </div>
-                    <div className="dropdown dropdown-end">
-                        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                            <div className="w-10 rounded-full">
-
-                                <img src="https://placeimg.com/80/80/people" alt='' />
-                            </div>
-                        </label>
-                        <div>{user?.email}</div>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    {user?.email ?
 
 
-                            <li onClick={handelLogout}>Logout</li>
-                        </ul>
-                    </div>
+                        <div className="dropdown dropdown-end">
+                            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                                <div className="w-10 rounded-full">
+
+                                    <img src="https://placeimg.com/80/80/people" alt='' />
+                                </div>
+                            </label>
+                            <div>{user?.email}</div>
+                            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+
+
+                                <li onClick={handelLogout}>Logout</li>
+                            </ul>
+                        </div>
+
+                        : <Link className="btn btn-ghost normal-case text-base" to='/login'>Login</Link>}
                 </div>
             </div>
         </div>
